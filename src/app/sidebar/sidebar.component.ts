a5ea2c612a01ccf7596105f9a7ce72b18336144c
Neema <neemawanjiru23@gmail.com>
2024-06-19 00:20:40 +0300

    sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
+  imports: [MatCardModule,MatIconModule,MatListModule, MatDividerModule],
 })
 export class SidebarComponent { }
